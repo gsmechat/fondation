71829599434812ff222a1ed2950931e63af9ef67
mechat_g <mechat_g@b3d5a8ed-ae99-4402-9d51-05b63b276f9b>
2016-03-21 11:06:23 +0000

    add modfication sur Makefile header main

--- header.h
+++ header.h
@@ -5,12 +5,17 @@
 ** Login   <mechat_g@etna-alternance.net>
 ** 
 ** Started on  Tue Feb  9 04:35:10 2016 MECHAT Guillaume
-** Last update Mon Mar 21 11:22:48 2016 MECHAT Guillaume
+** Last update Mon Mar 21 11:58:34 2016 MECHAT Guillaume
 */
 
 #ifndef HEADER_H_
 # define HEADER_H_
 
+#define SYNTAX_ERROR "Syntax Error: ./fondation --ip 192.168.1.1 --port 4242"
+
+int		my_strcmp(char *s1, char *s2);
+void		my_putchar(char c);
+void		my_putstr(char *str);
 
 
 #endif
