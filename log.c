7828a6e3edb48ad7115607167116bca23f2cd921
mechat_g <mechat_g@b3d5a8ed-ae99-4402-9d51-05b63b276f9b>
2016-04-25 15:33:59 +0000

    add norme

--- log.c
+++ log.c
@@ -5,23 +5,23 @@
 ** Login   <mechat_g@etna-alternance.net>
 ** 
 ** Started on  Wed Apr  6 19:50:13 2016 MECHAT Guillaume
-** Last update Wed Apr  6 19:50:40 2016 MECHAT Guillaume
+** Last update Thu Apr 21 14:00:24 2016 MECHAT Guillaume
 */
 
 #include "header.h"
 
-void    error()
+void	error()
 {
-  char  *action;
+  char	*action;
 
   action = "Connection à Fondation";
   perror(action);
   error_log(action);
 }
 
-int     error_log(char *txt)
+int	error_log(char *txt)
 {
-  int   id;
+  int	id;
 
   id = open("log.txt", O_APPEND|O_CREAT|O_RDWR, 777);
   write_log(txt, id);
@@ -32,7 +32,7 @@ int     error_log(char *txt)
   return (0);
 }
 
-int             write_log(char *txt, int id)
+int	write_log(char *txt, int id)
 {
   write(id, txt, my_strlen(txt));
   return (0);
